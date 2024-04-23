d5df107ea31fc4eb24c998dbf000b9c77eb98426
Orilles <167873998+Orilles@users.noreply.github.com>
2024-04-23 14:06:02 +0200

    abrir y cerrar pinza

--- principal/principal.ino
+++ principal/principal.ino
@@ -1,8 +1,27 @@
-//Pelayo estuvo aquí. (●'◡'●) fosky y pelayo estuvieron aquí (-0-)
-//Onichan estuvo aquí
-//El padre de Fueyo estuvo aquí
-//samu calvo
+#include <Servo.h>
+#include <Braccio.h>
 
+
+// Crear objetos de servos para cada articulación
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
+
+// Función para abrir la pinza del Braccio
+void openGripper() {
+ Braccio.ServoMovement(500, base.read(), shoulder.read(), elbow.read(), 
+                       wrist_rot.read(), wrist_ver.read(), 10);
+}
+
+// Función para cerrar la pinza del Braccio
+void closeGripper() {
+ Braccio.ServoMovement(500, base.read(), shoulder.read(), elbow.read(), 
+                       wrist_rot.read(), wrist_ver.read(), 73);
+}
 void setup() {
   // put your setup code here, to run once:
 
