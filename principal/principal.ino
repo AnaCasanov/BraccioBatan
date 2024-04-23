59088d20cce262154246f34d7c0b1383485b61a1
Orilles <167873998+Orilles@users.noreply.github.com>
2024-04-23 13:52:06 +0200

    Update principal.ino

--- principal/principal.ino
+++ principal/principal.ino
@@ -1,5 +1,6 @@
 //Pelayo estuvo aquí. (●'◡'●) fosky y pelayo estuvieron aquí (-0-)
 //Onichan estuvo aquí
+//El padre de Fueyo estuvo aquí
 void setup() {
   // put your setup code here, to run once:
 
