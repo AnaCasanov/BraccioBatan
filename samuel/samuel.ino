c08cf13b27a1d9e96e151a0b4c51336a8a140787
Xinales <sergiosc29@educastur.es>
2024-04-25 14:13:36 +0200

    Testeo

--- samuel/samuel.ino
+++ samuel/samuel.ino
@@ -1,20 +1,29 @@
 
 #include <Braccio.h>
 #include <Servo.h> 
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
   const int base_home= 0;
-  const int shoulder_home= 40;
+  const int shoulder_home= 80;
   const int elbow_home= 180;
   const int wrist_rot_home= 0;
   const int wrist_ver_home= 170;
   const int gripper_home= 72;
 
 void mover_posIni() {
-  Braccio.ServoMovement( base_home, shoulder_home, elbow_home, wrist_rot_home, wrist_ver_home, gripper_home );
+  Braccio.ServoMovement(220, base_home, shoulder_home, elbow_home, wrist_rot_home, wrist_ver_home, gripper_home );
 }
 
 
 void setup(){
- Braccio.ServoMovement(); 
+  Braccio.begin();
+  mover_posIni(); 
 }
 
 
--- sergio/sergio.ino
+++ sergio/sergio.ino
@@ -1,9 +1,59 @@
-void setup() {
-  // put your setup code here, to run once:
+#include <Braccio.h>
+#include <Servo.h>
+
+Servo base;
+Servo shoulder;
+Servo elbow;
+Servo wrist_rot;
+Servo wrist_ver;
+Servo gripper;
+
+  const int base_home= 0;
+  const int shoulder_home= 40;
+  const int elbow_home= 180;
+  const int wrist_rot_home= 0;
+  const int wrist_ver_home= 170;
+  const int gripper_home= 72;
+
+
+void coger_bola() {
+
+  Braccio.ServoMovement(200, base.read(), 80, elbow.read(), wrist_rot.read(), wrist_ver.read(), 10);
 
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void cerrar_pinza() {
 
+  Braccio.ServoMovement(200, base.read(), shoulder.read(), elbow.read(), wrist_rot.read(), wrist_ver.read(), 72);
+
+}
+
+void mover_pos_tirar() {
+
+  Braccio.ServoMovement(200, base.read(), shoulder.read(), 80, wrist_rot.read(), 100, gripper.read());
+
+}
+
+
+void tirar_bola() {
+
+  Braccio.ServoMovement(30, base.read(), shoulder.read(), 220, wrist_rot.read(), 40, 10);
+
+}
+
+void setup() {
+
+  Braccio.begin();
+  coger_bola();
+  delay(1000);
+  cerrar_pinza();
+  delay(1000);
+  mover_pos_tirar();
+  delay(2000);
+  tirar_bola();
+}
+
+void loop() {
+
+  
 }
